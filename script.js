6f623c7ea2cbee01f5cd023faf9d7ad5340196b6
maxicasanova <maxi1602@gmail.com>
2022-02-01 16:20:46 -0300

    prueba de juego autonomo

--- script.js
+++ script.js
@@ -21,11 +21,14 @@ let btnComenzar = document.getElementById("botonModal");
 
 const circulo = "https://img.icons8.com/emoji/48/000000/cross-mark-emoji.png";
 const cruz = "https://img.icons8.com/ios-filled/50/26e07f/circled.png";
+const delay = ms => new Promise(res => setTimeout(res, ms));
 let turno = 0;
 let forma1 = "";
 let forma2 = "";
 let player1 = '';
-let player2 = '';
+let player2 = 'Skynet';
+let ganador = "";
+let dificultad = 2
 
 
 // cuando se aprieta boton de inicio se abre modal de inicio
@@ -62,7 +65,7 @@ function reinicio(){
     forma1 = "";
     forma2 = "";
     player1 = '';
-    player2 = '';
+    ganador = "";
 
     for (let i = 0; i < 9; i++) {
         document.getElementById(`tarjetas${(i+1)}`).style.backgroundImage = '';
@@ -91,7 +94,7 @@ window.onclick = function(event) {
 function comenzar(){
 
     player1 = document.querySelector('#player1').value;
-    player2 = document.querySelector('#player2').value;
+    // player2 = document.querySelector('#player2').value;
     let selection = document.querySelector('input[name="razon"]:checked').value;
 
     // nombres de jugadores y seleccion de figura
@@ -129,6 +132,7 @@ function comenzar(){
         document.getElementById('queJugador').innerHTML = `Es el turno de: ${player2}`;
         document.getElementById('queJugador').style.display = 'block';
         turno = 2;
+        juegoAutonomo(dificultad)
     }
 
 }
@@ -154,7 +158,7 @@ const condicionGanar = [
 // funcion que detecta cuando terminar el juego
 
 function comprobacion() {
-    let ganador = "";
+
     for (let i = 0; i < 8; i++) {
         condA = condicionGanar[i][0];
         condB = condicionGanar[i][1];
@@ -177,6 +181,7 @@ function comprobacion() {
         }
     }
     if (bloques.bloq1 == 1 && bloques.bloq2 == 1 && bloques.bloq3 == 1 && bloques.bloq4 == 1 && bloques.bloq5 == 1 && bloques.bloq6 == 1 && bloques.bloq7 == 1 && bloques.bloq8 == 1 && bloques.bloq9 == 1 && ganador === '') {
+        ganador = 'empate';
         document.getElementById('jugadorGanador').innerHTML = "Empate";
         document.getElementById('jugadorGanador').style.display = 'block';
         modal2.style.display = "block";
@@ -197,6 +202,7 @@ function juego(casilla){
             turno = 2;
             document.getElementById('queJugador').innerHTML = `Es el turno de: ${player2}`;
             comprobacion();
+            juegoAutonomo(dificultad)
 
             } else{
             document.getElementById(`tarjetas${casilla}`).style.backgroundImage = `url("${forma2}")`;
@@ -212,3 +218,78 @@ function juego(casilla){
     }
     
 }
+
+const juegoAutonomo = async (dificultad) => {
+    await delay(2500);
+    if (ganador == ''){
+        if (dificultad == 1){
+            aleatorio = Math.round(Math.random() * 9);
+            while (bloques[`bloq${aleatorio}`] == 1) {
+                aleatorio = Math.round(Math.random() * 9);
+            }
+            juego(aleatorio);
+        } else if (dificultad == 2){
+            while (turno == 2) {
+                check = 0;
+                checkElegido = 0;
+                while (check == 0) {
+                    for (let i = 0; i < 8; i++) {
+                        condA = condicionGanar[i][0];
+                        condB = condicionGanar[i][1];
+                        condC = condicionGanar[i][2];
+
+                        if (signo[`bloq${condA}`] !== "" && signo[`bloq${condB}`] === signo[`bloq${condA}`]){
+                            if (signo[`bloq${condC}`] !== ""){
+                                continue;
+                            }
+                            juego(condC);
+                            check = 1;
+                            checkElegido = 1;
+                            break;
+                        } else if (signo[`bloq${condA}`] !== "" && signo[`bloq${condC}`] === signo[`bloq${condA}`]){
+                            if (signo[`bloq${condB}`] !== ""){
+                                continue;
+                            }
+                            juego(condB);
+                            check = 1;
+                            checkElegido = 1;
+                            break;
+                        } else if (signo[`bloq${condC}`] !== "" && signo[`bloq${condB}`] === signo[`bloq${condC}`]){
+                            if (signo[`bloq${condA}`] !== ""){
+                                continue;
+                            }
+                            juego(condA);
+                            check = 1;
+                            checkElegido = 1;
+                            break;
+                        } else {
+                            continue;
+                        }
+                    }
+                    check = 1;
+                }
+                if (checkElegido == 0){
+                    if (bloques.bloq5 != 1){
+                        jugada = 5;
+                        juego(jugada);
+                    } else if ( bloques.bloq1 != 1 || bloques.bloq3 != 1 || bloques.bloq7 != 1 || bloques.bloq9 != 1){
+                        for (let i = 1; i < 10; i=i+2) {
+                            if (bloques[`bloq${i}`] != 1){
+                                jugada = i;
+                                juego(jugada);
+                                break;
+                            }
+                        }
+                    } else{
+                        jugada = Math.round(Math.random() * 9);
+                        while (bloques[`bloq${jugada}`] == 1) {
+                            jugada = Math.round(Math.random() * 9);
+                        }
+                        juego(jugada);
+                    }
+                }
+            }
+        }
+    }
+}
+
